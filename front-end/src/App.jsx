3a19c39a1e4d8425f36b374d7d63d2e370cf38d8
ahmethedev <ahmethedev@gmail.com>
2024-03-07 00:54:39 +0300

    Initial Commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,30 @@
+import styles from "./style";
+import { Billing, Business, CardDeal, CTA, Footer, Navbar, Hero } from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Business />
+        <Billing />
+        <CardDeal />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ front-end/src/components/index.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import CTA from "./CTA";
+
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  CTA,
+  Footer,
+  Hero,
+};
