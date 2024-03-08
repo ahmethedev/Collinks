c469ba1ef8b776b4ce24449bb22c0afc933bf054
ahmethedev <ahmethedev@gmail.com>
2024-03-08 16:18:58 +0300

    Texts updated

--- front-end/src/components/Hero.jsx
+++ front-end/src/components/Hero.jsx
@@ -8,18 +8,16 @@ const Hero = () => {
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+            No more <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Endless</span>{" "}
           </h1>
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
+          Tabs.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+        Are you tired of drowning in a sea of open browser tabs, never quite getting around to reading the articles and resources you've saved for later? <span className="text-gradient">Collinks</span>{" "} is here to revolutionize the way you manage online content. Designed by professionals for professionals, Collinks is your personal assistant for conquering information overload and reclaiming your time.
         </p>
       </div>
 
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -9,7 +9,8 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      {/*todo */}
+      <img src={logo} alt="collinks" className="w-[124px] h-[32px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
