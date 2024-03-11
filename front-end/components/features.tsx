78d312cf53f2e298ad29d78fdc3475e438ba51ef
ahmethedev <ahmethedev@gmail.com>
2024-03-12 02:26:55 +0300

    frontend update

--- front-end/components/features.tsx
+++ front-end/components/features.tsx
@@ -6,8 +6,8 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 className="h2 mb-4">No more endless tabs.</h2>
+            <p className="text-xl text-gray-400">Are you tired of drowning in a sea of open browser tabs, never quite getting around to reading the articles and resources you've saved for later? Collinks is here to revolutionize the way you manage online content. Designed by professionals for professionals, Collinks is your personal assistant for conquering information overload and reclaiming your time.</p>
           </div>
 
           {/* Items */}
@@ -20,8 +20,8 @@ export default function Features() {
                 <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Organize Digital Universe</h4>
+              <p className="text-lg text-gray-400 text-center">Say goodbye to endless tabs and scattered bookmarks. With Collinks, you can effortlessly organize your favorite articles, blog posts, videos, and resources into customizable folders and reading lists.</p>
             </div>
 
             {/* 2nd item */}
@@ -31,8 +31,8 @@ export default function Features() {
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Never Miss a Read</h4>
+              <p className="text-lg text-gray-400 text-center">How many times have you saved an interesting article only to forget about it later? Collinks keeps you on track with intelligent reminders, ensuring that you revisit and engage with your saved content at the perfect moment.</p>
             </div>
 
             {/* 3rd item */}
@@ -45,8 +45,8 @@ export default function Features() {
                   <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Collaborate and Share</h4>
+              <p className="text-lg text-gray-400 text-center">Share your curated reading lists with friends, colleagues, or clients to spark discussions, exchange insights, and collaborate on projects. Harness the collective knowledge of your network and discover new perspectives together.</p>
             </div>
 
             {/* 4th item */}
@@ -58,8 +58,8 @@ export default function Features() {
                   <circle className="stroke-current text-purple-300" cx="13" cy="9" r="3" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">AI-Powered Insights</h4>
+              <p className="text-lg text-gray-400 text-center">Upgrade to Collinks Premium and unlock advanced features like AI-powered summaries and audio playback. Quickly grasp the key points of articles with concise summaries or listen to content on the go, transforming your reading experience.</p>
             </div>
 
             {/* 5th item */}
